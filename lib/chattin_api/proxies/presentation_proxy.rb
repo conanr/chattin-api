a6275b527cc669df6e5e46564a5d7a48745dc556
Conan Rimmer <conan@livingsocial.com>
2012-06-13 08:13:16 -0400

    rework presenter methods to return presenters for a given presentation

--- lib/chattin_api/proxies/presentation_proxy.rb
+++ lib/chattin_api/proxies/presentation_proxy.rb
@@ -10,9 +10,13 @@ module ChattinApi
       def find_all
         connection.get "/presentations"
       end
-
+      
       def create(attributes)
         connection.post "/presentations", attributes
+      end
+      
+      def find_all_presenters_for(id)
+        connection.get "/presentations/#{id}/presenters"
       end
     end  
   end
